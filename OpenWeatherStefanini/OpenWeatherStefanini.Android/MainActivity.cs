240ec854efa8b021e18a37d62e985b2e2b430479
Kevin Jhonson <kevinjh07@outlook.com>
2019-10-07 08:16:24 -0300

    Updated app icon, use viewcell on CitiesPage.

--- OpenWeatherStefanini/OpenWeatherStefanini.Android/MainActivity.cs
+++ OpenWeatherStefanini/OpenWeatherStefanini.Android/MainActivity.cs
@@ -6,7 +6,7 @@ using Prism.Ioc;
 
 namespace OpenWeatherStefanini.Droid
 {
-    [Activity(Label = "Open Weather", Icon = "@mipmap/ic_launcher", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "Open Weather", Icon = "@mipmap/ic_launcher", Theme = "@style/MainTheme", MainLauncher = false, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
---
+++ OpenWeatherStefanini/OpenWeatherStefanini.Android/SplashActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.OS;
+
+namespace OpenWeatherStefanini.Droid
+{
+    [Activity(Label = "Open Weather", Icon = "@mipmap/ic_launcher", Theme = "@style/SplashTheme", MainLauncher = true, NoHistory = true,
+        ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class SplashActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            StartActivity(typeof(MainActivity));
+            Finish();
+            OverridePendingTransition(0, 0);
+        }
+    }
+}
